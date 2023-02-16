c8bb9cf93a4a26aef34df96ce2d486eae6203a69
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-02-16 16:56:12 +0300

    Update App.vue
    Added components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup>
 import { RouterView } from "vue-router";
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+import SidenavComponent from "@/components/SidenavComponent.vue";
 </script>
 
 <template>
-  <RouterView />
+  <HeaderComponent />
+  <div class="flex flex-1 flex-col sm:flex-row">
+    <main
+      class="flex-1 sm:-ml-64 md:ml-0 bg-slate-300 text-slate-800 dark:bg-slate-900 dark:text-slate-200"
+    >
+      <RouterView />
+      <FooterComponent />
+    </main>
+    <SidenavComponent />
+  </div>
 </template>
