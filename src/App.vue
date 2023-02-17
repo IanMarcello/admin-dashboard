9f70b342745e253bbfdef4cc469ee353610288eb
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-02-17 17:07:58 +0300

    Update App.vue
    minor changes

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,16 @@ import { RouterView } from "vue-router";
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import FooterComponent from "@/components/FooterComponent.vue";
 import SidenavComponent from "@/components/SidenavComponent.vue";
+// import { useToggleStore } from "@/stores/state";
+
+// const toggleStore = useToggleStore();
 </script>
 
 <template>
   <HeaderComponent />
-  <div class="flex flex-1 flex-col sm:flex-row">
+  <div class="flex flex-col flex-1 sm:flex-row">
     <main
-      class="flex-1 sm:-ml-64 md:ml-0 bg-slate-300 text-slate-800 dark:bg-slate-900 dark:text-slate-200"
+      class="flex-1 bg-slate-100 sm:-ml-64 md:mt-0 md:ml-0 dark:bg-slate-900"
     >
       <RouterView />
       <FooterComponent />
