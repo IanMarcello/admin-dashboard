be7af92ab55378e10051380ad15d1504b88bb8ed
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-02-18 14:18:40 +0300

    Update App.vue
    Added toggle sidenav

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,18 @@ import { RouterView } from "vue-router";
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import FooterComponent from "@/components/FooterComponent.vue";
 import SidenavComponent from "@/components/SidenavComponent.vue";
-// import { useToggleStore } from "@/stores/state";
+import { useToggleStore } from "@/stores/state";
 
-// const toggleStore = useToggleStore();
+const toggleStore = useToggleStore();
 </script>
 
 <template>
   <HeaderComponent />
   <div class="flex flex-col flex-1 sm:flex-row">
     <main
-      class="flex-1 bg-slate-100 sm:-ml-64 md:mt-0 md:ml-0 dark:bg-slate-900"
+      :onclick="toggleStore.show ? toggleStore.toggleShow : ''"
+      class="flex-1 mt-20 bg-slate-100 sm:-ml-64 sm:mt-0 md:ml-0 dark:bg-slate-900"
+      :class="toggleStore.show ? 'brightness-50 select-none' : 'filter-none'"
     >
       <RouterView />
       <FooterComponent />
