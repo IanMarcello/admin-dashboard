b824ae7a124cf4c8e247ca12264ba9394e724fae
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-02-18 14:18:12 +0300

    Update DashboardView.vue
    Added grid responsiveness

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,44 +1,44 @@
 <script setup></script>
 
 <template>
-  <div class="grid gap-4">
-    <section class="grid grid-cols-12 gap-4 m-3">
+  <div class="grid gap-2 m-4">
+    <section class="grid grid-rows-2 gap-4 sm:grid-cols-12 sm:grid-rows-none">
       <div
-        class="col-span-8 p-3 overflow-auto border rounded-lg h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="p-3 overflow-auto border rounded-lg sm:col-span-8 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
         Grid One
       </div>
       <div
-        class="col-span-4 p-3 overflow-auto border rounded-lg h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
         Grid Two
       </div>
     </section>
-    <section class="grid grid-cols-12 gap-4 m-3">
+    <section class="grid grid-rows-3 gap-4 sm:grid-cols-12 sm:grid-rows-none">
       <div
-        class="col-span-4 p-3 overflow-auto border rounded-lg h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
         Grid Three
       </div>
       <div
-        class="col-span-4 p-3 overflow-auto border rounded-lg h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
         Grid Four
       </div>
       <div
-        class="col-span-4 p-3 overflow-auto border rounded-lg h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
         Grid Five
       </div>
     </section>
-    <section class="grid grid-cols-12 gap-4 m-3">
+    <section class="grid grid-rows-2 gap-4 sm:grid-cols-12 sm:grid-rows-none">
       <div
-        class="col-span-6 p-3 overflow-auto border rounded-lg h-68 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="p-3 overflow-auto border rounded-lg sm:col-span-6 h-68 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
         Grid Six
       </div>
       <div
-        class="grid col-span-6 grid-rows-2 p-3 overflow-auto border rounded-lg gap-y-4 h-fit border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800"
+        class="grid grid-rows-2 p-3 overflow-auto border rounded-lg sm:col-span-6 gap-y-4 h-fit border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800"
       >
         <div
           class="p-3 overflow-auto border rounded-lg h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
@@ -52,19 +52,19 @@
         </div>
       </div>
     </section>
-    <section class="grid grid-cols-12 gap-4 m-3">
+    <section class="grid grid-rows-2 gap-4 sm:grid-cols-12 sm:grid-rows-none">
       <div
-        class="col-span-6 p-3 overflow-auto border rounded-lg h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="p-3 overflow-auto border rounded-lg sm:col-span-6 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
         Grid Nine
       </div>
       <div
-        class="col-span-6 p-3 overflow-auto border rounded-lg h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="p-3 overflow-auto border rounded-lg sm:col-span-6 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
         Grid Ten
       </div>
     </section>
-    <section class="grid m-3">
+    <section class="grid">
       <div
         class="p-3 overflow-auto border rounded-lg h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
