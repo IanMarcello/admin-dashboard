6de3f65093600ac5b235f23b01a1353ced06c707
IanMarcello <ian.marcello.jada19@gmail.com>
2023-02-19 23:15:23 +0300

    Create auth.js

---
+++ src/stores/auth.js
@@ -0,0 +1,58 @@
+import { defineStore } from "pinia";
+import http from "@/services/http";
+
+export const useAuthStore = defineStore("auth", {
+  state: () => {
+    return {
+      id: null,
+      name: "",
+      email: "",
+      token: null,
+    };
+  },
+
+  actions: {
+    async login() {
+      const response = await http.post("/api/AuthAccount/Login", {
+        email: this.email,
+        password: this.password,
+      });
+
+      sessionStorage.setItem(
+        "user",
+        JSON.stringify({
+          id: response.data.data.Id,
+          name: response.data.data.Name,
+          email: this.email,
+          token: response.data.data.Token,
+        })
+      );
+
+      this.email = "";
+      this.password = "";
+
+      return response;
+    },
+
+    async register() {
+      if (this.password !== this.confirm_password) return false;
+
+      const response = await http.post("/api/AuthAccount/Register", {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      });
+
+      this.name = "";
+      this.email = "";
+      this.password = "";
+      this.confirm_password = "";
+
+      return response;
+    },
+
+    async logout() {
+      sessionStorage.clear();
+    },
+  },
+});
