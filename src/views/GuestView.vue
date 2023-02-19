e4b1720cc738ffb60da03e4e15bfe8570a1b2633
IanMarcello <ian.marcello.jada19@gmail.com>
2023-02-19 23:15:17 +0300

    Create base views

---
+++ src/views/GuestView.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <main class="flex flex-1 px-2 -mt-44 sm:-mt-0 items-center justify-center">
+    <h1
+      class="capitalize mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl"
+    >
+      {{ $t("guest_view") }}
+    </h1>
+  </main>
+</template>
