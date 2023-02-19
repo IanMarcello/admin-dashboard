421e48cdd8d07f26693b298f6bb85c9d83d6daa1
IanMarcello <ian.marcello.jada19@gmail.com>
2023-02-19 23:16:12 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DashboardView from "@/views/DashboardView.vue";
 
+// function guardRoute(to, from, next) {
+//   var isAuthenticated = false;
+
+//   if (sessionStorage.getItem("user")) {
+//     isAuthenticated = true;
+//   } else {
+//     isAuthenticated = false;
+//   }
+
+//   if (isAuthenticated) {
+//     next();
+//   } else {
+//     next({ name: "login" });
+//   }
+// }
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
