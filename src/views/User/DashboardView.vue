033ec5fad2e0cb2d3b56e7b0411fb60e89462f92
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-03-03 17:07:29 +0300

    Created views

---
+++ src/views/User/DashboardView.vue
@@ -0,0 +1,162 @@
+<script setup>
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { PieChart } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+} from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
+import { ref, provide } from "vue";
+
+const user = JSON.parse(sessionStorage.getItem("user")).name;
+
+use([
+  CanvasRenderer,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+]);
+
+provide(THEME_KEY, "dark");
+
+const option = ref({
+  title: {
+    text: "Dummy Info",
+    left: "center",
+    top: "20",
+  },
+  tooltip: {
+    trigger: "item",
+    formatter: "{a} <br/>{b} : {c} ({d}%)",
+  },
+  legend: {
+    orient: "vertical",
+    bottom: "bottom",
+    data: ["Direct", "Email", "Ad Networks", "Video Ads", "Search Engines"],
+  },
+  series: [
+    {
+      name: "Traffic Sources",
+      type: "pie",
+      radius: "55%",
+      center: ["50%", "60%"],
+      data: [
+        { value: 335, name: "Direct" },
+        { value: 310, name: "Email" },
+        { value: 234, name: "Ad Networks" },
+        { value: 135, name: "Video Ads" },
+        { value: 1548, name: "Search Engines" },
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: "rgba(0, 0, 0, 0.5)",
+        },
+      },
+    },
+  ],
+});
+
+// document.body.addEventListener("click", function (e) {
+//   if (e.target.nodeName === "BUTTON") {
+//     e.preventDefault();
+//     // myChart.dispose();
+//     // myChart = echarts.init(document.getElementById("chart"), e.target.id);
+//     // myChart.setOption(initOption);
+//     // myChart.setOption(chartData);
+//   }
+// });
+</script>
+
+<template>
+  <div class="grid gap-2 m-5">
+    <section class="grid">
+      <div class="p-3 h-fit overflow-auto text-slate-900 dark:text-slate-200">
+        <!-- {{ $t("dashboard.grid") }}{{ $t("dashboard.number.zero") }} -->
+        <p class="text-base font-semibold text-indigo-600">
+          {{ $t("user.welcome") }},
+        </p>
+        <h1
+          class="text-4xl font-bold tracking-tight text-slate-900 dark:text-slate-200 capitalize sm:text-3xl"
+        >
+          {{ user }}
+        </h1>
+      </div>
+    </section>
+    <section class="grid grid-rows-2 gap-4 sm:grid-cols-12 sm:grid-rows-none">
+      <div id="chartOne" class="overflow-auto sm:col-span-8 rounded-lg">
+        <v-chart
+          :option="option"
+          class="chart h-96 w-full bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+          autoresize
+        />
+      </div>
+      <div
+        class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+      >
+        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.two") }}
+      </div>
+    </section>
+    <section class="grid grid-rows-3 gap-4 sm:grid-cols-12 sm:grid-rows-none">
+      <div
+        class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+      >
+        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.three") }}
+      </div>
+      <div
+        class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+      >
+        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.four") }}
+      </div>
+      <div
+        class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+      >
+        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.five") }}
+      </div>
+    </section>
+    <section class="grid grid-rows-2 gap-4 sm:grid-cols-12 sm:grid-rows-none">
+      <div
+        class="p-3 overflow-auto border rounded-lg sm:col-span-6 h-68 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+      >
+        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.six") }}
+      </div>
+      <div
+        class="grid grid-rows-2 p-3 overflow-auto border rounded-lg sm:col-span-6 gap-y-4 h-fit border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800"
+      >
+        <div
+          class="p-3 overflow-auto border rounded-lg h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        >
+          {{ $t("dashboard.grid") }}{{ $t("dashboard.number.seven") }}
+        </div>
+        <div
+          class="p-3 overflow-auto border rounded-lg h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        >
+          {{ $t("dashboard.grid") }}{{ $t("dashboard.number.eight") }}
+        </div>
+      </div>
+    </section>
+    <section class="grid grid-rows-2 gap-4 sm:grid-cols-12 sm:grid-rows-none">
+      <div
+        class="p-3 overflow-auto border rounded-lg sm:col-span-6 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+      >
+        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.nine") }}
+      </div>
+      <div
+        class="p-3 overflow-auto border rounded-lg sm:col-span-6 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+      >
+        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.ten") }}
+      </div>
+    </section>
+    <section class="grid">
+      <div
+        class="p-3 overflow-auto border rounded-lg h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+      >
+        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.eleven") }}
+      </div>
+    </section>
+  </div>
+</template>
---
+++ src/views/User/DummyView6_1.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex items-center justify-center flex-1 px-2">
+    <h1
+      class="mt-2 text-4xl font-bold tracking-tight uppercase text-slate-900 dark:text-slate-200 sm:text-5xl"
+    >
+      {{ $t("content.dummySix1") }}
+    </h1>
+  </div>
+</template>
