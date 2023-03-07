b224c1ee864d643d9a5e3629a23104d23feb69b4
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-03-07 17:01:51 +0300

    Update DashboardView.vue
    Added notifications and some other changes

--- src/views/User/DashboardView.vue
+++ src/views/User/DashboardView.vue
@@ -1,38 +1,48 @@
 <script setup>
-import { ref } from "vue";
+// import { ref } from "vue";
 import { notify } from "notiwind";
+import { onMounted } from "vue";
 
-const user = JSON.parse(sessionStorage.getItem("session")).name;
+onMounted(() => {
+  const session = sessionStorage.getItem("session");
 
-notify(
-  {
-    group: "foo",
-    title: "Success",
-    text: "Your account was registered!",
-  },
-  4000
-); // 4s
+  if (!session) {
+    notify(
+      {
+        bg: "bg-green-500",
+        type: "success",
+        color: "text-green-500",
+        group: "foo",
+        title: "Success",
+        text: "Login successful!",
+      },
+      2000
+    );
+  }
 
-const options = ref({
-  chart: {
-    id: "vuechart-example",
-  },
-  xaxis: {
-    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-  },
-  theme: {
-    mode: "dark",
-  },
+  sessionStorage.setItem("session", true);
 });
 
-const series = ref([
-  {
-    name: "series-1",
-    data: [30, 40, 45, 50, 49, 60, 70, 91],
-  },
-]);
+const user = JSON.parse(sessionStorage.getItem("user")).name;
 
-// const user = JSON.parse(sessionStorage.getItem("user")).name;
+// const options = ref({
+//   chart: {
+//     id: "vuechart-example",
+//   },
+//   xaxis: {
+//     categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+//   },
+//   theme: {
+//     mode: "dark",
+//   },
+// });
+
+// const series = ref([
+//   {
+//     name: "series-1",
+//     data: [30, 40, 45, 50, 49, 60, 70, 91],
+//   },
+// ]);
 </script>
 
 <template>
@@ -51,18 +61,21 @@ const series = ref([
       </div>
     </section>
     <section class="grid grid-rows-2 gap-4 sm:grid-cols-12 sm:grid-rows-none">
-      <div
+      <!-- <div
         class="p-3 overflow-auto border rounded-lg sm:col-span-8 h-96 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800"
+      > -->
+      <div
+        class="p-3 overflow-auto border rounded-lg sm:col-span-8 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
-        <!-- {{ $t("dashboard.grid") }}{{ $t("dashboard.number.one") }} -->
-        <apexchart
+        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.one") }}
+        <!-- <apexchart
           class="text-slate-900 dark:text-slate-200"
           width="100%"
           height="100%"
           type="bar"
           :options="options"
           :series="series"
-        ></apexchart>
+        ></apexchart> -->
       </div>
       <div
         class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
