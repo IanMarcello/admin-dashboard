d40f14d8f3e206798ddcd8692800492cfb3ca9fc
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-03-10 16:51:04 +0300

    Added fade out in animation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { RouterView } from "vue-router";
 import NotificationComponent from "@/components/NotificationComponent.vue";
-
 </script>
 
 <template>
   <NotificationComponent />
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </RouterView>
 </template>
