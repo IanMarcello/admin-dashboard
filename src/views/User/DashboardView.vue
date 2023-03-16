6e738211e08edd65c5e04868f67651e9621f61de
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-03-16 17:09:37 +0300

    Update DashboardView.vue

--- src/views/User/DashboardView.vue
+++ src/views/User/DashboardView.vue
@@ -1,7 +1,17 @@
 <script setup>
-// import { ref } from "vue";
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { PieChart } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+} from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
+import { ref, provide, computed } from "vue";
 import { notify } from "notiwind";
 import { onMounted } from "vue";
+import { useThemeStore } from "@/stores/theme";
 
 onMounted(() => {
   const session = sessionStorage.getItem("session");
@@ -23,26 +33,60 @@ onMounted(() => {
   sessionStorage.setItem("session", true);
 });
 
+const themeStore = useThemeStore();
 const user = JSON.parse(sessionStorage.getItem("user")).name;
 
-// const options = ref({
-//   chart: {
-//     id: "vuechart-example",
-//   },
-//   xaxis: {
-//     categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-//   },
-//   theme: {
-//     mode: "dark",
-//   },
-// });
+use([
+  CanvasRenderer,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+]);
 
-// const series = ref([
-//   {
-//     name: "series-1",
-//     data: [30, 40, 45, 50, 49, 60, 70, 91],
-//   },
-// ]);
+provide(
+  THEME_KEY,
+  computed(() => themeStore.theme_key)
+);
+
+const option = ref({
+  title: {
+    text: "Traffic Sources",
+    left: "center",
+  },
+  tooltip: {
+    trigger: "item",
+    formatter: "{a} <br/>{b} : {c} ({d}%)",
+  },
+  legend: {
+    orient: "horizontal",
+    left: "center",
+    bottom: "bottom",
+    data: ["Direct", "Email", "Ad Networks", "Video Ads", "Search Engines"],
+  },
+  series: [
+    {
+      name: "Traffic Sources",
+      type: "pie",
+      radius: "70%",
+      center: ["50%", "50%"],
+      data: [
+        { value: 335, name: "Direct" },
+        { value: 310, name: "Email" },
+        { value: 234, name: "Ad Networks" },
+        { value: 135, name: "Video Ads" },
+        { value: 1548, name: "Search Engines" },
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: "rgba(0, 0, 0, 0.5)",
+        },
+      },
+    },
+  ],
+});
 </script>
 
 <template>
@@ -65,39 +109,98 @@ const user = JSON.parse(sessionStorage.getItem("user")).name;
         class="p-3 overflow-auto border rounded-lg sm:col-span-8 h-96 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800"
       > -->
       <div
-        class="p-3 overflow-auto border rounded-lg sm:col-span-8 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="p-3 overflow-auto rounded-lg sm:col-span-8 h-96 shadow-2xl bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
         {{ $t("dashboard.grid") }}{{ $t("dashboard.number.one") }}
-        <!-- <apexchart
-          class="text-slate-900 dark:text-slate-200"
-          width="100%"
-          height="100%"
-          type="bar"
-          :options="options"
-          :series="series"
-        ></apexchart> -->
       </div>
       <div
-        class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="overflow-auto rounded-lg sm:col-span-4 h-96 shadow-2xl bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
-        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.two") }}
+        <!-- {{ $t("dashboard.grid") }}{{ $t("dashboard.number.two") }} -->
+        <v-chart class="chart" :option="option" autoresize :key="key" />
       </div>
     </section>
     <section class="grid grid-rows-3 gap-4 sm:grid-cols-12 sm:grid-rows-none">
       <div
-        class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="overflow-auto rounded-lg sm:col-span-4 h-fit shadow-2xl bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
-        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.three") }}
+        <!-- {{ $t("dashboard.grid") }}{{ $t("dashboard.number.three") }} -->
+        <div
+          class="flex divide-x-2 divide-slate-400 dark:divide-slate-200 h-full w-full"
+        >
+          <div class="flex flex-col items-start px-5 w-3/4 py-6">
+            <div class="mb-3">
+              <span>Title 1</span>
+            </div>
+
+            <div class="text-2xl">
+              <span>12435454</span>
+            </div>
+          </div>
+
+          <div
+            class="flex flex-col items-center justify-center w-1/4 px-5 py-6"
+          >
+            <img
+              src="https://api.iconify.design/ic:baseline-account-circle.svg"
+              class="w-12 h-12 bg-slate-200 dark:bg-slate-800"
+            />
+          </div>
+        </div>
       </div>
       <div
-        class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="overflow-auto rounded-lg sm:col-span-4 h-fit shadow-lg bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
-        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.four") }}
+        <!-- {{ $t("dashboard.grid") }}{{ $t("dashboard.number.four") }} -->
+        <div
+          class="flex divide-x-2 divide-slate-400 dark:divide-slate-200 h-full w-full"
+        >
+          <div class="flex flex-col items-start px-5 w-3/4 py-6">
+            <div class="mb-3">
+              <span>Title 2</span>
+            </div>
+
+            <div class="text-2xl">
+              <span>12435454</span>
+            </div>
+          </div>
+
+          <div
+            class="flex flex-col items-center justify-center w-1/4 px-5 py-6"
+          >
+            <img
+              src="https://api.iconify.design/ic:baseline-account-circle.svg"
+              class="w-12 h-12 bg-slate-200 dark:bg-slate-800"
+            />
+          </div>
+        </div>
       </div>
       <div
-        class="p-3 overflow-auto border rounded-lg sm:col-span-4 h-44 border-slate-400 dark:border-slate-500 bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
+        class="overflow-auto rounded-lg sm:col-span-4 h-fit shadow-lg bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-200"
       >
-        {{ $t("dashboard.grid") }}{{ $t("dashboard.number.five") }}
+        <!-- {{ $t("dashboard.grid") }}{{ $t("dashboard.number.five") }} -->
+        <div
+          class="flex divide-x-2 divide-slate-400 dark:divide-slate-200 h-full w-full"
+        >
+          <div class="flex flex-col items-start px-5 w-3/4 py-6">
+            <div class="mb-3">
+              <span>Title 3</span>
+            </div>
+
+            <div class="text-2xl">
+              <span>12435454</span>
+            </div>
+          </div>
+
+          <div
+            class="flex flex-col items-center justify-center w-1/4 px-5 py-6"
+          >
+            <img
+              src="https://api.iconify.design/ic:baseline-account-circle.svg"
+              class="w-12 h-12 bg-slate-200 dark:bg-slate-800"
+            />
+          </div>
+        </div>
       </div>
     </section>
     <section class="grid grid-rows-2 gap-4 sm:grid-cols-12 sm:grid-rows-none">
