33087eddfc008687886d1427576c2493f52011ee
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-03-30 17:00:22 +0300

    Updated store

--- src/stores/state.js
+++ src/stores/state.js
@@ -2,11 +2,13 @@ import { defineStore } from "pinia";
 
 export const useToggleStore = defineStore("toggle", {
   state: () => ({
-    show: false,
+    show: true,
+    openSidebar: true,
   }),
   actions: {
     toggleShow() {
       this.show = !this.show;
+      this.openSidebar = !this.openSidebar;
     },
   },
 });
