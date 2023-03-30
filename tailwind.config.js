fd0865e4d7260a9ed41e1be78c0b33edcd2acc5b
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-03-30 16:59:55 +0300

    Updated config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,36 @@
-/** @type {import('tailwindcss').Config} */
+/* eslint-disable no-undef */
 module.exports = {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          50: "#ffffff",
+          100: "#f8f9fa",
+          200: "#e9ecef",
+          300: "#dee2e6",
+          400: "#ced4da",
+          500: "#adb5bd",
+          600: "#6c757d",
+          700: "#495057",
+          800: "#343a40",
+          900: "#212529",
+        },
+        dark: {
+          50: "#0081c6",
+          100: "#0073b0",
+          200: "#00649a",
+          300: "#005684",
+          400: "#00486e",
+          500: "#003958",
+          600: "#002b42",
+          700: "#001d2c",
+          800: "#000e16",
+          900: "#000000",
+        },
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
 };
