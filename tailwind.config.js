f0ad77186ad94febbc91127428bb3312ec7feaac
IanMarcello <ian.marcello.jada19@gmail.com>
2023-04-11 00:03:27 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,5 +32,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
+  plugins: [
+    require("tailwind-scrollbar")({ nocompatible: true }),
+    require("@tailwindcss/forms"),
+  ],
 };
