408c327bf13c65bcf753cc0de7dd6f9a62dac5a3
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-04-11 17:07:31 +0300

    Updated component

--- src/components/icons/profileIcon.vue
+++ src/components/icons/profileIcon.vue
@@ -19,7 +19,7 @@ const logout = async () => {
   <Menu as="div" class="relative inline-block">
     <div>
       <MenuButton>
-        <span
+        <!-- <span
           class="inline-block w-10 h-10 overflow-hidden bg-gray-100 rounded-full cursor-pointer"
         >
           <svg
@@ -31,6 +31,14 @@ const logout = async () => {
               d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"
             />
           </svg>
+        </span> -->
+        <span
+          class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-light-300 dark:bg-dark-400"
+        >
+          <span
+            class="font-medium leading-none text-dark-900 dark:text-light-200"
+            >IM</span
+          >
         </span>
       </MenuButton>
     </div>
