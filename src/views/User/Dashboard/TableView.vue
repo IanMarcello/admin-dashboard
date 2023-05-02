375b09f9d9cc462760b911208840df15173f6f1d
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-05-02 17:41:01 +0300

    Updated views

--- src/views/User/Dashboard/TableView.vue
+++ src/views/User/Dashboard/TableView.vue
@@ -1,3 +1,55 @@
+<script setup>
+import { dateFormatter, numberFormatter } from "@/services/formatter";
+
+const transactions = [
+  {
+    description: "Payment to Ian Marcello",
+    date: "2023-04-19",
+    amount: 114436,
+    reference_number: 3822833082,
+    payment_method: "Bank",
+    status: "Completed",
+    color: "bg-green-200",
+  },
+  {
+    description: "Payment to Ian Marcello",
+    date: "2023-04-08",
+    amount: 273589,
+    reference_number: 8385461565,
+    payment_method: "Bank",
+    status: "Pending",
+    color: "bg-orange-200",
+  },
+  {
+    description: "Payment to Ian Marcello",
+    date: "2023-04-05",
+    amount: 722683,
+    reference_number: 2843531097,
+    payment_method: "Bank",
+    status: "Cancelled",
+    color: "bg-red-200",
+  },
+  {
+    description: "Payment to Ian Marcello",
+    date: "2023-03-29",
+    amount: 508182,
+    reference_number: 3977181730,
+    payment_method: "Bank",
+    status: "Completed",
+    color: "bg-green-200",
+  },
+  {
+    description: "Payment to Ian Marcello",
+    date: "2023-03-13",
+    amount: 814383,
+    reference_number: 3822112624,
+    payment_method: "Bank",
+    status: "Completed",
+    color: "bg-green-200",
+  },
+];
+</script>
+
 <template>
   <div class="px-4 sm:px-6 lg:px-8">
     <div class="sm:flex sm:items-center">
@@ -7,7 +59,9 @@
       </div>
     </div>
     <div class="mt-5 flex flex-col">
-      <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div
+        class="-my-2 -mx-4 overflow-x-auto scrollbar-thin scrollbar-track-blue-300 scrollbar-thumb-blue-800 sm:-mx-6 lg:-mx-8"
+      >
         <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
           <table class="min-w-full divide-y divide-gray-300">
             <thead>
@@ -61,12 +115,10 @@
                   {{ transaction.description }}
                 </td>
                 <td class="whitespace-nowrap py-4 px-3 text-sm">
-                  {{ transaction.date }}
-                  <!-- {{ $d(transaction.date, "content", "en") }} -->
+                  {{ dateFormatter.format(new Date(transaction.date)) }}
                 </td>
                 <td class="whitespace-nowrap py-4 px-3 text-sm">
-                  {{ transaction.amount }}
-                  <!-- {{ $n(transaction.amount, "decimal", "ja-JP") }} -->
+                  {{ numberFormatter.format(transaction.amount) }}
                 </td>
                 <td class="whitespace-nowrap py-4 px-3 text-sm">
                   {{ transaction.reference_number }}
@@ -89,53 +141,3 @@
     </div>
   </div>
 </template>
-
-<script setup>
-const transactions = [
-  {
-    description: "Payment to Ian Marcello",
-    date: "19-04-2023",
-    amount: 114436,
-    reference_number: 3822833082,
-    payment_method: "Member",
-    status: "Completed",
-    color: "bg-green-200",
-  },
-  {
-    description: "Payment to Ian Marcello",
-    date: "08-04-2023",
-    amount: 273589,
-    reference_number: 8385461565,
-    payment_method: "Member",
-    status: "Pending",
-    color: "bg-orange-200",
-  },
-  {
-    description: "Payment to Ian Marcello",
-    date: "05-04-2023",
-    amount: 722683,
-    reference_number: 2843531097,
-    payment_method: "Member",
-    status: "Cancelled",
-    color: "bg-red-200",
-  },
-  {
-    description: "Payment to Ian Marcello",
-    date: "29-03-2023",
-    amount: 508182,
-    reference_number: 3977181730,
-    payment_method: "Member",
-    status: "Completed",
-    color: "bg-green-200",
-  },
-  {
-    description: "Payment to Ian Marcello",
-    date: "13-03-2023",
-    amount: 814383,
-    reference_number: 3822112624,
-    payment_method: "Member",
-    status: "Completed",
-    color: "bg-green-200",
-  },
-];
-</script>
--- src/views/User/UsersView.vue
+++ src/views/User/UsersView.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="py-3 px-4 sm:px-6 lg:px-8">
+  <div
+    class="py-3 px-4 sm:px-6 lg:px-8 bg-light-100 dark:bg-dark-600 text-slate-900 dark:text-slate-200"
+  >
     <div class="sm:flex sm:items-center">
       <div class="sm:flex-auto">
-        <h1 class="text-xl font-semibold text-gray-900">Users</h1>
-        <p class="mt-2 text-sm text-gray-700">
+        <h1 class="text-xl font-semibold">Users</h1>
+        <p class="mt-2 text-sm">
           A list of all the users in your account including their name, title,
           email and role.
         </p>
@@ -11,7 +13,7 @@
       <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
         <button
           type="button"
-          class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+          class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
         >
           Add user
         </button>
@@ -22,48 +24,48 @@
         <div class="inline-block min-w-full py-2 align-middle">
           <div class="shadow-sm ring-1 ring-black ring-opacity-5">
             <table class="min-w-full border-separate" style="border-spacing: 0">
-              <thead class="bg-gray-50">
+              <thead>
                 <tr>
                   <th
                     scope="col"
-                    class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
+                    class="sticky top-16 sm:top-0 z-10 border-b border-gray-300 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
                   >
                     Name
                   </th>
                   <th
                     scope="col"
-                    class="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
+                    class="sticky top-16 sm:top-0 z-10 hidden border-b border-gray-300 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold backdrop-blur backdrop-filter sm:table-cell"
                   >
                     Title
                   </th>
                   <th
                     scope="col"
-                    class="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
+                    class="sticky top-16 sm:top-0 z-10 hidden border-b border-gray-300 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold backdrop-blur backdrop-filter lg:table-cell"
                   >
                     Email
                   </th>
                   <th
                     scope="col"
-                    class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                    class="sticky top-16 sm:top-0 z-10 border-b border-gray-300 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold backdrop-blur backdrop-filter"
                   >
                     Role
                   </th>
                   <th
                     scope="col"
-                    class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pr-4 pl-3 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8"
+                    class="sticky top-16 sm:top-0 z-10 border-b border-gray-300 bg-opacity-75 py-3.5 pr-4 pl-3 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8"
                   >
                     <span class="sr-only">Edit</span>
                   </th>
                 </tr>
               </thead>
-              <tbody class="bg-white">
+              <tbody>
                 <tr v-for="(person, personIdx) in people" :key="person.email">
                   <td
                     :class="[
                       personIdx !== people.length - 1
                         ? 'border-b border-gray-200'
                         : '',
-                      'whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8',
+                      'whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6 lg:pl-8',
                     ]"
                   >
                     {{ person.name }}
@@ -73,7 +75,7 @@
                       personIdx !== people.length - 1
                         ? 'border-b border-gray-200'
                         : '',
-                      'whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden sm:table-cell',
+                      'whitespace-nowrap px-3 py-4 text-sm hidden sm:table-cell',
                     ]"
                   >
                     {{ person.title }}
@@ -83,7 +85,7 @@
                       personIdx !== people.length - 1
                         ? 'border-b border-gray-200'
                         : '',
-                      'whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden lg:table-cell',
+                      'whitespace-nowrap px-3 py-4 text-sm hidden lg:table-cell',
                     ]"
                   >
                     {{ person.email }}
@@ -93,7 +95,7 @@
                       personIdx !== people.length - 1
                         ? 'border-b border-gray-200'
                         : '',
-                      'whitespace-nowrap px-3 py-4 text-sm text-gray-500',
+                      'whitespace-nowrap px-3 py-4 text-sm',
                     ]"
                   >
                     {{ person.role }}
