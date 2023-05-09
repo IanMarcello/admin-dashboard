0931d569465a0a2a08a7a55be0cf2174f9a7754d
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-05-09 16:54:46 +0300

    Updated components

--- src/components/icons/menuIcon.vue
+++ src/components/icons/menuIcon.vue
@@ -1,19 +1,49 @@
 <script setup>
 import { useToggleStore } from "@/stores/state";
-import IcRoundMenu from "~icons/ic/round-menu";
+// import IcRoundMenu from "~icons/ic/round-menu";
 
 const toggleStore = useToggleStore();
 </script>
 
 <template>
-  <a
+  <!-- <a
     class="flex items-start justify-center order-last p-2 rounded-lg dark:bg-[#002b42] hover:bg-[#f8f9fa] dark:hover:bg-[#002b42]"
-  >
-    <span
-      @click.prevent="toggleStore.toggleShow"
-      class="text-slate-900 dark:text-slate-200"
+  > -->
+  <!-- <span
+    @click.prevent="toggleStore.toggleShow"
+    class="text-slate-900 dark:text-slate-200"
+  > -->
+  <!-- <IcRoundMenu class="w-6 h-6" /> -->
+  <button class="relative" @click.prevent="toggleStore.toggleShow">
+    <div
+      class="relative flex items-center rounded-lg hover:bg-[#f8f9fa] dark:hover:bg-[#002b42] justify-center w-10 h-10 transform transition-all"
     >
-      <IcRoundMenu class="w-6 h-6" />
-    </span>
-  </a>
+      <div
+        class="flex flex-col justify-between w-4 h-4 overflow-hidden transform transition-all duration-500 origin-center"
+        :class="[toggleStore.openSidebar ? '-rotate-180' : 'rotate-180']"
+      >
+        <div
+          class="bg-dark-500 dark:bg-light-300 h-[2px] w-6 transform transition-all duration-500"
+          :class="[
+            toggleStore.openSidebar
+              ? ''
+              : '-rotate-45 -translate-x-1 translate-y-[0rem]',
+          ]"
+        ></div>
+        <div
+          class="bg-dark-500 dark:bg-light-300 h-[2px] w-6 rounded transform transition-all duration-500"
+        ></div>
+        <div
+          class="bg-dark-500 dark:bg-light-300 h-[2px] w-6 transform transition-all duration-500"
+          :class="[
+            toggleStore.openSidebar
+              ? ''
+              : 'rotate-45 -translate-x-1 translate-y-[0rem]',
+          ]"
+        ></div>
+      </div>
+    </div>
+  </button>
+  <!-- </span> -->
+  <!-- </a> -->
 </template>
