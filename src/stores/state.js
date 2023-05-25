40eb1e64c40b97338c953b985fa2d6015bc2353b
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-05-25 15:10:32 +0300

    Update state.js
    Added new variable in toggle store

--- src/stores/state.js
+++ src/stores/state.js
@@ -3,10 +3,14 @@ import { defineStore } from "pinia";
 export const useToggleStore = defineStore("toggle", {
   state: () => ({
     openSidebar: true,
+    openList: true,
   }),
   actions: {
     toggleShow() {
       this.openSidebar = !this.openSidebar;
+    },
+    toggleList() {
+      this.openList = !this.openList;
     },
   },
 });
